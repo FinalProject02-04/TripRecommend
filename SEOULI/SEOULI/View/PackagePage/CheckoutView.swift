645c40070b01a6f2d9fa6e3857d49afc22228216
kimsor1 <imoiksr@gmail.com>
2024-06-25 15:44:53 +0900

    Swift 프로젝트 생성

---
+++ SEOULI/SEOULI/View/PackagePage/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  SEOULI
+//
+//  Created by 김소리 on 6/25/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
---
+++ SEOULI/SEOULI/View/ProfilePage/EditInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  EditInfoView.swift
+//  SEOULI
+//
+//  Created by 김소리 on 6/25/24.
+//
+
+import SwiftUI
+
+struct EditInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditInfoView()
+}
