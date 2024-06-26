e6fdcab6eef154c7f372f54cea8b040ea58bae07
kimsor1 <imoiksr@gmail.com>
2024-06-26 17:58:34 +0900

    0626 / packageList, packageDetail Page 작업 및 요청 페이지 추가

---
+++ SEOULI/SEOULI/Model/ProductModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProductModel.swift
+//  SEOULI
+//
+//  Created by 김소리 on 6/26/24.
+//
+
+import Foundation
+
+struct ProductModel : Identifiable{
+    var id = UUID()
+    var name: String
+    var image: String
+    var price : String
+}
