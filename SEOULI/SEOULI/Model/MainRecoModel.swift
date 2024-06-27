1719dc83c1bb916b20e000b910f5bea68c792e7a
kimsor1 <imoiksr@gmail.com>
2024-06-27 10:32:28 +0900

    서씨 요청으로 postModel 추가, 수민 요청으로 MainRecoModel 삭제

--- SEOULI/SEOULI/Model/MainRecoModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MainRecoModel.swift
-//  SEOULI
-//
-//  Created by 김소리 on 6/26/24.
-//
-
-import Foundation
---
+++ SEOULI/SEOULI/Model/PostModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostModel.swift
+//  SEOULI
+//
+//  Created by 김소리 on 6/27/24.
+//
+
+import Foundation
