8e4829a942a11bedc01458bc1060f61bfbf36476
lslh1994 <lh1994@naver.com>
2024-07-08 16:54:04 +0900

    24.07.08_16:53
    본인게시물 외 타게시물 상세페이지 수정 및 삭제 아이콘 비활성화 구현 (최종)

--- SEOULI/SEOULI/View/CommunityPage/PostDetailView.swift
+++ SEOULI/SEOULI/View/CommunityPage/PostDetailView.swift
@@ -25,150 +25,145 @@ struct PostDetailView: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(editedTitle)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
+        ZStack {
+            Color("Background Color") // 배경색 설정
+                .ignoresSafeArea() // Safe Area를 무시하고 배경색이 전체 화면에 적용되도록 함
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(editedTitle)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("Text Color")) // 사용자 지정 텍스트 색상 사용
+                            .multilineTextAlignment(.leading)
+                        
+                        HStack {
+                            Text("\(community.date)")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            
+                            Text(" \(community.username)")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.top, 4)
+                    }
+                    
+                    Text(editedSubtitle)
+                        .font(.body)
                         .foregroundColor(Color("Text Color")) // 사용자 지정 텍스트 색상 사용
                         .multilineTextAlignment(.leading)
                     
-                    HStack {
-                        Text("\(community.date)")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                        
-                        Text(" \(community.username)")
-                            .font(.subheadline)
-                            .fontWeight(.bold)
+                    Text(editedContent)
+                        .font(.body)
+                        .foregroundColor(Color("Text Color")) // 사용자 지정 텍스트 색상 사용
+                        .multilineTextAlignment(.leading)
+                    
+                    if !editedImage.isEmpty {
+                        loadImage(named: editedImage)
+                            .map {
+                                Image(uiImage: $0)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 350, height: 300)
+                                    .clipped()
+                            }
+                    }
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .background(Color.clear) // 배경색 설정
+            }
+        }
+        .navigationBarTitle("", displayMode: .inline)
+        .navigationBarItems(trailing:
+            Group {
+                if UserDefaults.standard.string(forKey: "userNickname") == community.username {
+                    Button(action: {
+                        showingActionSheet = true
+                    }) {
+                        Image(systemName: "ellipsis.circle")
+                            .font(.title)
                             .foregroundColor(.gray)
                     }
-                    .padding(.top, 4)
-                }
-                
-                Text(editedSubtitle)
-                    .font(.body)
-                    .foregroundColor(Color("Text Color")) // 사용자 지정 텍스트 색상 사용
-                    .multilineTextAlignment(.leading)
-                
-                Text(editedContent)
-                    .font(.body)
-                    .foregroundColor(Color("Text Color")) // 사용자 지정 텍스트 색상 사용
-                    .multilineTextAlignment(.leading)
-                
-                if !editedImage.isEmpty {
-                    loadImage(named: editedImage)
-                        .map {
-                            Image(uiImage: $0)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 350, height: 300)
-                                .clipped()
-                        }
-                }
-                
-                Spacer()
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding()
-            .background(Color("Background Color")) // 배경색 설정
-            .ignoresSafeArea()
-            .navigationBarTitle("", displayMode: .inline)
-            .navigationBarItems(trailing:
-                Button(action: {
-                    showingActionSheet = true
-                }) {
-                    Image(systemName: "ellipsis.circle")
-                        .font(.title)
-                        .foregroundColor(.gray)
-                }
-                .actionSheet(isPresented: $showingActionSheet) {
-                    ActionSheet(title: Text("게시물 관리"), message: nil, buttons: [
-                        .default(Text("수정하기")) {
-                            // 사용자 권한 확인
-                            if UserDefaults.standard.string(forKey: "userNickname") == community.username {
-                                // 수정하기 버튼을 누른 경우의 처리
+                    .actionSheet(isPresented: $showingActionSheet) {
+                        ActionSheet(title: Text("게시물 관리"), message: nil, buttons: [
+                            .default(Text("수정하기")) {
                                 showingEditView = true // 수정 화면을 표시하거나 다른 처리를 수행
-                            } else {
-                                // 사용자에게 권한이 없음을 알리는 처리
-                                // 예를 들어 경고창을 표시하거나 다른 방법으로 처리
-                            }
-                        },
-                        .destructive(Text("삭제하기")) {
-                            // 사용자 권한 확인
-                            if UserDefaults.standard.string(forKey: "userNickname") == community.username {
+                            },
+                            .destructive(Text("삭제하기")) {
                                 showingDeleteAlert = true
-                            } else {
-                                // 사용자에게 권한이 없음을 알리는 처리
-                                // 예를 들어 경고창을 표시하거나 다른 방법으로 처리
-                            }
-                        },
-                        .cancel()
-                    ])
-                }
-            )
-            .sheet(isPresented: $showingEditView) {
-                EditPostView(
-                    editedTitle: $editedTitle,
-                    editedSubtitle: $editedSubtitle,
-                    editedContent: $editedContent,
-                    editedImage: $editedImage,
-                    community: $community,
-                    postVM: postVM
-                ) {
-                    if let index = postData.communities.firstIndex(where: { $0.id == community.id }) {
-                        let updatedPost = PostModel(
-                            id: community.id,
-                            title: editedTitle,
-                            username: community.username,
-                            subtitle: editedSubtitle,
-                            content: editedContent,
-                            date: community.date,
-                            image: editedImage
-                        )
-                        postData.communities[index] = updatedPost
-                        
-                        postVM.updatePost(post: updatedPost) { error in
-                            if let error = error {
-                                print("게시물 업데이트 오류: \(error.localizedDescription)")
-                            } else {
-                                print("게시물이 성공적으로 업데이트되었습니다!")
-                                community = updatedPost
-                            }
-                        }
-                    }
-                } onDelete: {
-                    if let index = postData.communities.firstIndex(where: { $0.id == community.id }) {
-                        postData.communities.remove(at: index)
-                        presentationMode.wrappedValue.dismiss() // Navigate back to list
+                            },
+                            .cancel()
+                        ])
                     }
                 }
-                .environmentObject(postData)
             }
-            .alert("삭제 확인", isPresented: $showingDeleteAlert, actions: {
-                Button("확인", role: .destructive, action: {
-                    postVM.deletePost(post: community) { error in
+        )
+        .sheet(isPresented: $showingEditView) {
+            EditPostView(
+                editedTitle: $editedTitle,
+                editedSubtitle: $editedSubtitle,
+                editedContent: $editedContent,
+                editedImage: $editedImage,
+                community: $community,
+                postVM: postVM
+            ) {
+                if let index = postData.communities.firstIndex(where: { $0.id == community.id }) {
+                    let updatedPost = PostModel(
+                        id: community.id,
+                        title: editedTitle,
+                        username: community.username,
+                        subtitle: editedSubtitle,
+                        content: editedContent,
+                        date: community.date,
+                        image: editedImage
+                    )
+                    postData.communities[index] = updatedPost
+                    
+                    postVM.updatePost(post: updatedPost) { error in
                         if let error = error {
-                            print("게시물 삭제 오류: \(error.localizedDescription)")
+                            print("게시물 업데이트 오류: \(error.localizedDescription)")
                         } else {
-                            if let index = postData.communities.firstIndex(where: { $0.id == community.id }) {
-                                postData.communities.remove(at: index)
-                                presentationMode.wrappedValue.dismiss()
-                            }
+                            print("게시물이 성공적으로 업데이트되었습니다!")
+                            community = updatedPost
                         }
                     }
-                })
-                Button("취소", role: .cancel, action: {
-                    showingDeleteAlert = false
-                })
-            }, message: {
-                Text("정말 삭제하시겠습니까?")
-            })
-            .onChange(of: showingDeleteAlert) { showAlert in
-                if !showAlert {
-                    presentationMode.wrappedValue.dismiss() // Dismiss the sheet when the alert is dismissed
                 }
+            } onDelete: {
+                if let index = postData.communities.firstIndex(where: { $0.id == community.id }) {
+                    postData.communities.remove(at: index)
+                    presentationMode.wrappedValue.dismiss() // Navigate back to list
+                }
+            }
+            .environmentObject(postData)
+        }
+        .alert("삭제 확인", isPresented: $showingDeleteAlert, actions: {
+            Button("확인", role: .destructive, action: {
+                postVM.deletePost(post: community) { error in
+                    if let error = error {
+                        print("게시물 삭제 오류: \(error.localizedDescription)")
+                    } else {
+                        if let index = postData.communities.firstIndex(where: { $0.id == community.id }) {
+                            postData.communities.remove(at: index)
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+            })
+            Button("취소", role: .cancel, action: {
+                showingDeleteAlert = false
+            })
+        }, message: {
+            Text("정말 삭제하시겠습니까?")
+        })
+        .onChange(of: showingDeleteAlert) { showAlert in
+            if !showAlert {
+                presentationMode.wrappedValue.dismiss() // Dismiss the sheet when the alert is dismissed
             }
         }
     }
